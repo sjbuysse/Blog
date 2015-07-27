f7e4349378b99798f92b9372876ff0f1b2217156
Simon Buysse <buysse.simon@gmail.com>
2015-07-27 18:26:14 +0200

    added tests and code for post index action

---
+++ spec/features/posts/index_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper.rb"
+
+describe "Viewing posts" do
+    context "there are no posts" do
+        it "displays no posts" do
+            expect(Post.all.count).to eq(0)
+            visit "/posts" 
+            expect(page.all("div.post_wrapper").count).to eq(0)
+        end
+    end
+    context "there are posts" do
+        it "displays all posts" do
+            post = Post.create(title: "My first post", body: "This is some test text")
+            visit "/posts" 
+            expect(page.all("div.post_wrapper").count).to eq(1)
+        end
+    end
+end
+    
