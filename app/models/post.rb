1a80e2779f8737a722cdb9096fababd4dcd9dac2
Simon Buysse <buysse.simon@gmail.com>
2015-07-28 13:00:58 +0200

    modified descending query

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
     validates :title, presence: true, length: {minimum: 3}
     validates :body, presence: true, length: {minimum: 15}
 
-    scope :descending, -> {order('created_at ASC')}
+    scope :descending, -> {order('created_at DESC')}
 end
