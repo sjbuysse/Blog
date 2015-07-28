6b0adadcd3bea39a2e22819e9198fe2dcc35aebe
Simon Buysse <buysse.simon@gmail.com>
2015-07-28 19:51:23 +0200

    added sidebar, header and some styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
     validates :title, presence: true, length: {minimum: 3}
     validates :body, presence: true, length: {minimum: 15}
 
-    scope :descending, -> {order('created_at DESC')}
+    scope :descending, -> {order('created_at DESC').limit(4)}
 end
