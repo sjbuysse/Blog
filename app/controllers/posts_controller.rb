345314884bb30392ba3c86e7746f2066b1c83e8f
Simon Buysse <buysse.simon@gmail.com>
2015-07-28 12:59:16 +0200

    added descending query to post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     def index
-        @posts = Post.all
+        @posts = Post.descending
     end
 
     def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true, length: {minimum: 3}
     validates :body, presence: true, length: {minimum: 15}
+
+    scope :descending, -> {order('created_at ASC')}
 end
