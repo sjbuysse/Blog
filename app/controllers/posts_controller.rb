e4272555161514bf75a5437ff8dd25b5097a4d69
Simon Buysse <buysse.simon@gmail.com>
2015-07-29 22:23:47 +0200

    added tests and code for deleting posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,6 +37,14 @@ class PostsController < ApplicationController
         end
     end
 
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        
+        flash[:success] = "Successfully deleted post"
+        redirect_to root_path
+    end
+
     private
     def post_params
         params.require(:post).permit(:title, :body)
