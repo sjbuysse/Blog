82f28f203391713e68f7b0742329e0c0d9b79f67
Simon Buysse <buysse.simon@gmail.com>
2015-08-04 14:52:01 +0200

    refactored css a bit more modal, and completed comments feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+    before_action :find_post
+
+    def create
+        @comment = @post.comments.new(comment_params)
+        if @comment.save
+            redirect_to post_path(@post) 
+        else
+            render "posts/show"
+        end
+    end
+
+    def destroy
+        comment = @post.comments.find(params[:id])
+        if comment.destroy
+            redirect_to @post
+        else
+            flash[:error] = "couldn't delete comment"
+            render @post
+        end
+    end
+
+    private
+    def find_post
+        @post = Post.find(params[:post_id])
+    end
+
+    def comment_params
+        params[:comment].permit(:name, :body)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ActiveRecord::Base
-    has_many :comments
+    has_many :comments do 
+        def persisted
+            select(&:persisted?)
+        end
+    end
 
     validates :title, presence: true, length: {minimum: 3}
     validates :body, presence: true, length: {minimum: 15}
 
-    scope :descending, -> {order('created_at DESC').limit(4)}
+    scope :descending, -> {order('created_at DESC')}
 end
