e08d3b0c5dee3c6974c609fe9e00ac58e74dfb7b
Simon Buysse <buysse.simon@gmail.com>
2015-09-08 15:24:27 +0700

    deleted paperclip stuff in model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,10 +1,5 @@
 class Picture < ActiveRecord::Base
     belongs_to :post
 
-    has_attached_file :image,
-        :path => ":rails_root/public/images/:id/:filename",
-        :url  => "/images/:id/:filename"
-    
-    do_not_validate_attachment_file_type :image
-
+    mount_uploader :image, ImageUploader
 end
