21cf2a19736d73edc10d64664c3cf4c34fb2039f
Simon Buysse <buysse.simon@gmail.com>
2015-09-08 17:54:01 +0700

    debugged migration

--- db/migrate/20150908034055_rename_column_image_file_name_to_image.rb
+++ db/migrate/20150908034055_rename_column_image_file_name_to_image.rb
@@ -1,5 +1,5 @@
 class RenameColumnImageFileNameToImage < ActiveRecord::Migration
   def change
-      rename_column :picture, :image_file_name, :image
+      rename_column :pictures, :image_file_name, :image
   end
 end
