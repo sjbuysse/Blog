fd9a31fc320177fbd4c8003a3fe4276f74231d18
Simon Buysse <buysse.simon@gmail.com>
2015-09-10 12:24:30 +0700

    added cloudinary config

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
-  #include Cloudinary::CarrierWave
+  include Cloudinary::CarrierWave
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
