b5b918ceeec2b8ff8ab2e2fe212365c085ae37f4
Simon Buysse <buysse.simon@gmail.com>
2016-07-29 17:52:25 +1000

    Add bower configuration

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module BlogMackenzie
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.precompile += ['exclusive/*']
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end
