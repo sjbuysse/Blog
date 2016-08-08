95e6693917dde8efcca3249d2fb7fdd51351097f
Simon Buysse <buysse.simon@gmail.com>
2016-08-08 20:53:05 +1000

    Add auto dpr to cloudinary link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908085551) do
+ActiveRecord::Schema.define(version: 20160727080110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,12 +38,13 @@ ActiveRecord::Schema.define(version: 20150908085551) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "first_body"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "author"
     t.text     "synopsis"
     t.string   "thumb"
+    t.string   "second_body"
   end
 
   create_table "users", force: :cascade do |t|
